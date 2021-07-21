c13962510738ce0094694b93c9b925bfe5665687
Januar-Martinez <87335988+Januar-Martinez@users.noreply.github.com>
2021-07-21 16:56:53 -0500

    principios del menu de inicio

---
+++ discosRockoko/inicio.php
@@ -0,0 +1,28 @@
+<?php
+include 'encabezado.php';
+
+	echo "
+	<div class='row container' style = 'margin-top:100px;'>
+		<div class='col-2'>
+			<nav class='navbar navbar-light' style='background-color: #664747;'>
+				<div class='container-fluid'>
+					<ul class='navbar-nav'>
+						<li class='nav-item'>
+							<a class='nav-link active' style='color:#E6E6E6;font-face:' href='#'>Home</a>
+						</li>
+						<li class='nav-item'>
+							<a class='nav-link' style='color:#E6E6E6;font-face:' href='#'>Tutorials</a>
+						</li>
+						<li class='nav-item'>
+							<a class='nav-link' style='color:#E6E6E6;font-face:' href='#'>Articles</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		<div class='col-10'></div>
+	</div>
+	";
+
+
+include 'piePagina.php';
+?>  
